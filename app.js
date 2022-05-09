4c5e8107d018424d096fd18fe5c7749089d0042f
stanford30 <stanleyzheng84@gmail.com>
2022-05-08 20:15:54 -0500

    hello

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-const express = require("express");
-const morgan = require("morgan");
+const express = require('express');
+const morgan = require('morgan');
 
-const tourRouter = require("./routes/tourRoutes");
-const userRouter = require("./routes/userRoutes");
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
 // 1) MIDDLEWARES
-app.use(morgan("dev"));
+if (process.env.NODE_NEV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log("hello from the middleware");
+  console.log('hello from the middleware');
   next();
 });
 
@@ -22,7 +26,7 @@ app.use((req, res, next) => {
 
 //3) ROUTES
 
-app.use("/api/v1/users", userRouter);
-app.use("/api/v1/tours", tourRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/tours', tourRouter);
 
 module.exports = app;
